499acf5d621064c187372ba10dcb4709f859a651
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-05-11 17:56:22 +0530

    stored the task list in local storage

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,7 @@ import TodoList from './TodoList'
 
 const Todo = () => {
 
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]);
   const inputRef = useRef();
 
   const add = () => {
@@ -45,7 +45,7 @@ const Todo = () => {
   }
   
   useEffect(()=>{
-    console.log(todoList)
+    localStorage.setItem("todos",JSON.stringify(todoList));
   },[todoList])
 
   return (
